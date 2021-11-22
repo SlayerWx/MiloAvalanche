eb3ccebe00d47355bd17f74c96c9cf0c151c46ab
SlayerWx <maxi_cs_95@hotmail.com>
2021-11-22 11:11:33 -0300

    life script

---
+++ Assets/script/player/Life.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    public int initialHP;
+    private int hp;
+    void Start()
+    {
+        hp = initialHP;
+    }
+}
